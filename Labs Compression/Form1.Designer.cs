17ecaf3e4247f4cf47490d4b4d0811d268e279f5
kravenrus <47505285+kravenrus@users.noreply.github.com>
2021-01-20 07:28:55 +0400

    First release

---
+++ Labs Compression/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Labs_Compression
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSource = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textSource = new System.Windows.Forms.TextBox();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.groupAlgorithm = new System.Windows.Forms.GroupBox();
+            this.radioHUFFMAN = new System.Windows.Forms.RadioButton();
+            this.radioLZW = new System.Windows.Forms.RadioButton();
+            this.radioRLE = new System.Windows.Forms.RadioButton();
+            this.buttonCompress = new System.Windows.Forms.Button();
+            this.labelCompressTime = new System.Windows.Forms.Label();
+            this.labelDecompressTime = new System.Windows.Forms.Label();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.groupAlgorithm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelSource
+            // 
+            this.labelSource.Location = new System.Drawing.Point(12, 9);
+            this.labelSource.Name = "labelSource";
+            this.labelSource.Size = new System.Drawing.Size(274, 13);
+            this.labelSource.TabIndex = 0;
+            this.labelSource.Text = "Исходный текст";
+            this.labelSource.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelResult
+            // 
+            this.labelResult.Location = new System.Drawing.Point(292, 9);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(274, 13);
+            this.labelResult.TabIndex = 1;
+            this.labelResult.Text = "Текст после сжатия";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textSource
+            // 
+            this.textSource.Location = new System.Drawing.Point(12, 25);
+            this.textSource.Multiline = true;
+            this.textSource.Name = "textSource";
+            this.textSource.Size = new System.Drawing.Size(274, 152);
+            this.textSource.TabIndex = 2;
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(292, 25);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(274, 152);
+            this.textResult.TabIndex = 3;
+            // 
+            // groupAlgorithm
+            // 
+            this.groupAlgorithm.Controls.Add(this.radioHUFFMAN);
+            this.groupAlgorithm.Controls.Add(this.radioLZW);
+            this.groupAlgorithm.Controls.Add(this.radioRLE);
+            this.groupAlgorithm.Location = new System.Drawing.Point(12, 183);
+            this.groupAlgorithm.Name = "groupAlgorithm";
+            this.groupAlgorithm.Size = new System.Drawing.Size(134, 100);
+            this.groupAlgorithm.TabIndex = 4;
+            this.groupAlgorithm.TabStop = false;
+            this.groupAlgorithm.Text = "Алгоритм сжатия";
+            // 
+            // radioHUFFMAN
+            // 
+            this.radioHUFFMAN.AutoSize = true;
+            this.radioHUFFMAN.Location = new System.Drawing.Point(6, 65);
+            this.radioHUFFMAN.Name = "radioHUFFMAN";
+            this.radioHUFFMAN.Size = new System.Drawing.Size(77, 17);
+            this.radioHUFFMAN.TabIndex = 5;
+            this.radioHUFFMAN.Text = "HUFFMAN";
+            this.radioHUFFMAN.UseVisualStyleBackColor = true;
+            // 
+            // radioLZW
+            // 
+            this.radioLZW.AutoSize = true;
+            this.radioLZW.Location = new System.Drawing.Point(6, 42);
+            this.radioLZW.Name = "radioLZW";
+            this.radioLZW.Size = new System.Drawing.Size(49, 17);
+            this.radioLZW.TabIndex = 5;
+            this.radioLZW.Text = "LZW";
+            this.radioLZW.UseVisualStyleBackColor = true;
+            // 
+            // radioRLE
+            // 
+            this.radioRLE.AutoSize = true;
+            this.radioRLE.Checked = true;
+            this.radioRLE.Location = new System.Drawing.Point(6, 19);
+            this.radioRLE.Name = "radioRLE";
+            this.radioRLE.Size = new System.Drawing.Size(46, 17);
+            this.radioRLE.TabIndex = 0;
+            this.radioRLE.TabStop = true;
+            this.radioRLE.Text = "RLE";
+            this.radioRLE.UseVisualStyleBackColor = true;
+            // 
+            // buttonCompress
+            // 
+            this.buttonCompress.Location = new System.Drawing.Point(152, 188);
+            this.buttonCompress.Name = "buttonCompress";
+            this.buttonCompress.Size = new System.Drawing.Size(134, 95);
+            this.buttonCompress.TabIndex = 5;
+            this.buttonCompress.Text = "Сжать";
+            this.buttonCompress.UseVisualStyleBackColor = true;
+            this.buttonCompress.Click += new System.EventHandler(this.buttonCompress_Click);
+            // 
+            // labelCompressTime
+            // 
+            this.labelCompressTime.Location = new System.Drawing.Point(292, 188);
+            this.labelCompressTime.Name = "labelCompressTime";
+            this.labelCompressTime.Size = new System.Drawing.Size(274, 13);
+            this.labelCompressTime.TabIndex = 6;
+            this.labelCompressTime.Text = "Время компрессии:";
+            this.labelCompressTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelDecompressTime
+            // 
+            this.labelDecompressTime.Location = new System.Drawing.Point(292, 207);
+            this.labelDecompressTime.Name = "labelDecompressTime";
+            this.labelDecompressTime.Size = new System.Drawing.Size(274, 13);
+            this.labelDecompressTime.TabIndex = 7;
+            this.labelDecompressTime.Text = "Время декомпрессии:";
+            this.labelDecompressTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.labelCopyright.Location = new System.Drawing.Point(292, 270);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(274, 13);
+            this.labelCopyright.TabIndex = 8;
+            this.labelCopyright.Text = "Copyright ITSS-18 Maks Grishin";
+            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 293);
+            this.Controls.Add(this.labelCopyright);
+            this.Controls.Add(this.labelDecompressTime);
+            this.Controls.Add(this.labelCompressTime);
+            this.Controls.Add(this.buttonCompress);
+            this.Controls.Add(this.groupAlgorithm);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.textSource);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelSource);
+            this.Name = "MainForm";
+            this.Text = "Compression Assistant";
+            this.groupAlgorithm.ResumeLayout(false);
+            this.groupAlgorithm.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSource;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textSource;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.GroupBox groupAlgorithm;
+        private System.Windows.Forms.RadioButton radioHUFFMAN;
+        private System.Windows.Forms.RadioButton radioLZW;
+        private System.Windows.Forms.RadioButton radioRLE;
+        private System.Windows.Forms.Button buttonCompress;
+        private System.Windows.Forms.Label labelCompressTime;
+        private System.Windows.Forms.Label labelDecompressTime;
+        private System.Windows.Forms.Label labelCopyright;
+    }
+}
+
---
+++ Labs Compression/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs_Compression
+{
+    public partial class MainForm : Form
+    {
+        Timer timer = new Timer();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCompress_Click(object sender, EventArgs e)
+        {
+            if (radioRLE.Checked)
+            {
+
+                StringBuilder compressed = new StringBuilder();
+                StringBuilder number = new StringBuilder();
+                int count = 0; char currentSybmol = textSource.Text[0];
+
+                timer.Start();
+                foreach (char symbol in textSource.Text)
+                {
+                    if (symbol == currentSybmol) { count++; }
+                    else { compressed.Append(count.ToString() + currentSybmol); currentSybmol = symbol; count = 1; }
+                }
+                compressed.Append(count.ToString() + currentSybmol); textResult.Text = compressed.ToString(); compressed.Clear();
+                labelCompressTime.Text = "Время компрессии: " + timer.Stop() + "мс";
+
+                timer.Start();
+                foreach (char symbol in textResult.Text)
+                {
+                    if (char.IsDigit(symbol)) { number.Append(symbol); }
+                    else { for (int i = 0; i < int.Parse(number.ToString()); i++) { compressed.Append(symbol); } number.Clear(); }
+                }
+                textSource.Text = compressed.ToString(); compressed.Clear();
+                labelDecompressTime.Text = "Время декомпрессии: " + timer.Stop() + "мс";
+            }
+            if (radioLZW.Checked)
+            {
+                string w = string.Empty; var enc = Encoding.GetEncoding(1251);
+                List<int> compressed = new List<int>();
+                Dictionary<string, int> dictionaryCompress = new Dictionary<string, int>();
+                Dictionary<int, string> dictionaryDecompress = new Dictionary<int, string>();
+
+                timer.Start();
+                for (int i = 0; i < 256; i++) { dictionaryCompress.Add(enc.GetString(new[] { (byte)i }), i); }
+
+                foreach (char c in textSource.Text)
+                {
+                    string wc = w + c;
+
+                    if (dictionaryCompress.ContainsKey(wc)) { w = wc; }
+                    else { compressed.Add(dictionaryCompress[w]); dictionaryCompress.Add(wc, dictionaryCompress.Count); w = c.ToString(); }
+                }
+
+                if (!string.IsNullOrEmpty(w)) { compressed.Add(dictionaryCompress[w]); }
+
+                textResult.Text = string.Join(", ", compressed); labelCompressTime.Text = "Время компрессии: " + timer.Stop() + "мс";
+
+                timer.Start();
+                for (int i = 0; i < 256; i++) { dictionaryDecompress.Add(i, enc.GetString(new[] { (byte)i })); }
+
+                compressed = textResult.Text.Split(',').Select(Int32.Parse).ToList();
+
+                w = dictionaryDecompress[compressed[0]];
+                compressed.RemoveAt(0);
+                StringBuilder decompressed = new StringBuilder(w);
+
+                foreach (int k in compressed)
+                {
+                    string entry = null;
+
+                    if (dictionaryDecompress.ContainsKey(k)) { entry = dictionaryDecompress[k]; }
+                    else if (k == dictionaryDecompress.Count) { entry = w + w[0]; }
+
+                    decompressed.Append(entry); dictionaryDecompress.Add(dictionaryDecompress.Count, w + entry[0]); w = entry;
+                }
+                textSource.Text = decompressed.ToString(); labelDecompressTime.Text = "Время декомпрессии: " + timer.Stop() + "мс";
+            }
+            if (radioHUFFMAN.Checked)
+            {
+                StringBuilder compressed = new StringBuilder();
+                HuffmanTree huffmanTree = new HuffmanTree(); huffmanTree.Build(textSource.Text);
+                BitArray encoded = huffmanTree.Encode(textSource.Text);
+
+                foreach (bool bit in encoded) { compressed.Append((bit ? 1 : 0)); }
+
+                timer.Start(); textResult.Text = compressed.ToString(); labelCompressTime.Text = "Время компрессии: " + timer.Stop() + "мс";
+                timer.Start(); textSource.Text = huffmanTree.Decode(encoded); labelDecompressTime.Text = "Время декомпрессии: " + timer.Stop() + "мс";
+            }
+        }
+    }
+
+    public class Node
+    {
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+        public List<bool> Traverse(char symbol, List<bool> data)
+        {
+            if (Right == null && Left == null) { if (symbol.Equals(this.Symbol)) { return data; } else { return null; } }
+            else
+            {
+                List<bool> left = null; List<bool> right = null;
+
+                if (Left != null) { List<bool> leftPath = new List<bool>(); leftPath.AddRange(data); leftPath.Add(false); left = Left.Traverse(symbol, leftPath); }
+                if (Right != null) { List<bool> rightPath = new List<bool>(); rightPath.AddRange(data); rightPath.Add(true); right = Right.Traverse(symbol, rightPath); }
+                if (left != null) { return left; } else { return right; }
+            }
+        }
+    }
+
+    public class HuffmanTree
+    {
+        private List<Node> nodes = new List<Node>();
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public Node Root { get; set; }
+
+        public void Build(string source)
+        {
+            for (int i = 0; i < source.Length; i++) { if (!Frequencies.ContainsKey(source[i])) { Frequencies.Add(source[i], 0); } Frequencies[source[i]]++; }
+
+            foreach (KeyValuePair<char, int> symbol in Frequencies) { nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value }); }
+
+            while (nodes.Count > 1)
+            {
+                List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+
+                if (orderedNodes.Count >= 2)
+                {
+                    List<Node> taken = orderedNodes.Take(2).ToList<Node>();
+                    Node parent = new Node() { Symbol = '*', Frequency = taken[0].Frequency + taken[1].Frequency, Left = taken[0], Right = taken[1] };
+                    nodes.Remove(taken[0]); nodes.Remove(taken[1]); nodes.Add(parent);
+                }
+                this.Root = nodes.FirstOrDefault();
+            }
+        }
+
+        public BitArray Encode(string source)
+        {
+            List<bool> encodedSource = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+            BitArray bits = new BitArray(encodedSource.ToArray()); return bits;
+        }
+
+        public string Decode(BitArray bits)
+        {
+            Node current = this.Root; string decoded = "";
+
+            foreach (bool bit in bits)
+            {
+                if (bit && current.Right != null) { current = current.Right; }
+                else if (current.Left != null) { current = current.Left; }
+                if (IsLeaf(current)) { decoded += current.Symbol; current = this.Root; }
+            }
+            return decoded;
+        }
+
+        public bool IsLeaf(Node node) { return (node.Left == null && node.Right == null); }
+    }
+
+    public class Timer
+    {
+        DateTime timeStart, timeEnd = new DateTime();
+        TimeSpan timeDifference = new TimeSpan();
+
+        public void Start() { timeStart = DateTime.Now; }
+        public string Stop() { timeEnd = DateTime.Now; timeDifference = timeEnd - timeStart; return timeDifference.TotalMilliseconds.ToString(); }
+    }
+}
